de6a9df8612f93aec5ade22d30ea17280070d514
GustavMair <110348084+GustavMair@users.noreply.github.com>
2024-11-11 16:50:25 +0100

    Add files via upload

---
+++ Ja Nein Würfel/app.js
@@ -0,0 +1,9 @@
+let randomResult = 0
+randomResult = Math.round(Math.random())
+
+if (randomResult == 0){
+    document.write ("Ja")
+}
+else if (randomResult == 1){
+        document.write ("Nein")
+}
