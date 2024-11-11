15a3f6138e6627377495b5835fae99a0bf892206
GustavMair <110348084+GustavMair@users.noreply.github.com>
2024-11-11 16:49:36 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-let randomResult = 0
-randomResult = Math.round(Math.random())
-
-if (randomResult == 0){
-    document.write ("Ja")
-}
-else if (randomResult == 1){
-        document.write ("Nein")
-}
